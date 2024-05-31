50cc4bafe268d5ee1c099c659ed62122807c4a86
Georgi <radevg98@gmail.com>
2024-05-31 23:21:48 +0300

    Small improvements of the code quality

--- academy-bridger/src/Bridgerton.sol
+++ academy-bridger/src/Bridgerton.sol
@@ -13,9 +13,9 @@ contract Bridgerton is IBridgerton {
     mapping(address => uint256) public lockedBalance;
 
     event LockTokens(address indexed user, uint256 amount);
-    event UnlockTokens(address indexed user, uint256 amount, address indexed admin);
-    event MintWrappedToken(address indexed user, uint256 amount, address indexed admin);
-    event BurnWrappedToken(address indexed user, uint256 amount, address indexed admin);
+    event UnlockTokens(address indexed user, uint256 amount);
+    event MintWrappedToken(address indexed user, uint256 amount);
+    event BurnWrappedToken(address indexed user, uint256 amount);
 
     constructor(address _mainTokenAddress) {
         admin = msg.sender;
@@ -27,7 +27,7 @@ contract Bridgerton is IBridgerton {
     }
 
     modifier onlyAdmin() {
-        require(msg.sender == admin, "User is not an admin");
+        require(msg.sender == admin, "Only admin can call this function");
         _;
     }
 
@@ -43,6 +43,7 @@ contract Bridgerton is IBridgerton {
 
     function lockTokens(uint256 _amount) external nonZeroAmount(_amount) {
         require(address(mainToken) != address(0), "MainToken not initialized");
+
         require(mainToken.allowance(msg.sender, address(this)) >= _amount, "Allowance not set or insufficient");
         require(mainToken.balanceOf(msg.sender) >= _amount, "Insufficient token balance");
 
@@ -60,10 +61,10 @@ contract Bridgerton is IBridgerton {
     {
         require(address(mainToken) != address(0), "MainToken not initialized");
 
-        require(mainToken.transfer(_user, _amount), "Transfer failed");
         lockedBalance[_user] -= _amount;
+        require(mainToken.transfer(_user, _amount), "Transfer failed");
 
-        emit UnlockTokens(_user, _amount, msg.sender);
+        emit UnlockTokens(_user, _amount);
     }
 
     function mintWrappedTokens(address _user, uint256 _amount) external onlyAdmin nonZeroAmount(_amount) {
@@ -71,7 +72,7 @@ contract Bridgerton is IBridgerton {
 
         wrappedToken.mint(_user, _amount);
 
-        emit MintWrappedToken(_user, _amount, msg.sender);
+        emit MintWrappedToken(_user, _amount);
     }
 
     function burnWrappedTokens(address _user, uint256 _amount) external onlyAdmin nonZeroAmount(_amount) {
@@ -83,7 +84,7 @@ contract Bridgerton is IBridgerton {
 
         wrappedToken.burn(address(this), _amount);
 
-        emit BurnWrappedToken(_user, _amount, msg.sender);
+        emit BurnWrappedToken(_user, _amount);
     }
 
     function getLockedBalance(address _user) external view returns (uint256) {
